0dc156e93419859c5e84a5206097b2675fea8608
Joost de Folter <folterj@gmail.com>
2025-05-30 15:58:21 +0200

    Update run_hpc_EMPIAR12193.sh

--- run_hpc_EMPIAR12193.sh
+++ run_hpc_EMPIAR12193.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=muvis_align-empiar12193
 #SBATCH --part=ncpu
 #SBATCH --cpus-per-task=16
-#SBATCH --time=1-00:00          # Runtime in D-HH:MM, minimum of 10 minutes
+#SBATCH --time=5-00:00          # Runtime in D-HH:MM, minimum of 10 minutes
 #SBATCH --mem=640G   # Memory pool for all cores (see also --mem-per-cpu)
 
 export PYTHONUNBUFFERED=TRUE
