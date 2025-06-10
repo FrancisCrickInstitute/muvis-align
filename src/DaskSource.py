Fixed metadata sourcing, fixed image scaling, fixed metadata summary, removed old sources

src/DaskSource.py
@@ -1,10 +1,12 @@
 import numpy as np
 
-from src.util import get_value_units_micrometer
+from src.util import get_value_units_micrometer, find_all_numbers, split_numeric_dict, eval_context
 
 
 class DaskSource:
-    def __init__(self, filename):
+    default_physical_unit = 'µm'
+
+    def __init__(self, filename, source_metadata=None):
         self.filename = filename
         self.dimension_order = ''
         self.is_rgb = False
@@ -18,6 +20,42 @@ class DaskSource:
         self.position = {}
         self.rotation = 0
         self.channels = []
+        self.init_metadata()
+        self.fix_metadata(source_metadata)
+
+    def init_metadata(self):
+        raise NotImplementedError("Dask source should implement init_metadata() to initialize metadata")
+
+    def fix_metadata(self, source_metadata=None):
+        if isinstance(source_metadata, dict):
+            filename_numeric = find_all_numbers(self.filename)
+            filename_dict = {key: int(value) for key, value in split_numeric_dict(self.filename).items()}
+            context = {'filename_numeric': filename_numeric, 'fn': filename_numeric} | filename_dict
+            if 'position' in source_metadata:
+                translation0 = source_metadata['position']
+                if 'x' in translation0:
+                    self.position['x'] = eval_context(translation0, 'x', 0, context)
+                if 'y' in translation0:
+                    self.position['y'] = eval_context(translation0, 'y', 0, context)
+                if 'z' in translation0:
+                    self.position['z'] = eval_context(translation0, 'z', 0, context)
+            if 'scale' in source_metadata:
+                scale0 = source_metadata['scale']
+                if 'x' in scale0:
+                    self.pixel_size['x'] = eval_context(scale0, 'x', 1, context)
+                if 'y' in scale0:
+                    self.pixel_size['y'] = eval_context(scale0, 'y', 1, context)
+                if 'z' in scale0:
+                    self.pixel_size['z'] = eval_context(scale0, 'z', 1, context)
+            if 'rotation' in source_metadata:
+                self.rotation = source_metadata['rotation']
+
+        for shape in self.shapes:
+            scale1 = []
+            for dim in 'xy':
+                index = self.dimension_order.index(dim)
+                scale1.append(self.shape[index] / shape[index])
+            self.scales.append(np.mean(scale1))
 
     def get_shape(self, level=0):
         # shape in pixels
@@ -36,6 +74,11 @@ class DaskSource:
             pixel_size0 = get_value_units_micrometer(self.pixel_size)
             pixel_size = {dim: size * scale for dim, size in pixel_size0.items()}
         return pixel_size
+
+    def get_physical_size(self):
+        pixel_size = self.get_pixel_size()
+        size = self.get_size()
+        return {dim: size[dim] * pixel_size[dim] for dim in pixel_size.keys() if dim in size}
 
     def get_position(self, level=0):
         # position in micrometers
@@ -61,11 +104,3 @@ class DaskSource:
 
     def get_data(self, level=0):
         raise NotImplementedError()
-
-    def fix_metadata(self):
-        for shape in self.shapes:
-            scale1 = []
-            for dim in 'xy':
-                index = self.dimension_order.index(dim)
-                scale1.append(self.shape[index] / shape[index])
-            self.scales.append(np.mean(scale1))

src/MVSRegistration.py
@@ -105,7 +105,7 @@ class MVSRegistration:
             original_positions_filename = output + 'positions_original.pdf'
 
             with Timer('plot positions', self.logging_time):
-                vis_utils.plot_positions(sims.copy(), transform_key=self.source_transform_key,
+                vis_utils.plot_positions(sims, transform_key=self.source_transform_key,
                                          use_positional_colors=False, view_labels=file_labels, view_labels_size=3,
                                          show_plot=self.mpl_ui, output_filename=original_positions_filename)
 
@@ -192,7 +192,7 @@ class MVSRegistration:
 
         registered_positions_filename = output + 'positions_registered.pdf'
         with Timer('plot positions', self.logging_time):
-            vis_utils.plot_positions(sims.copy(), transform_key=self.reg_transform_key,
+            vis_utils.plot_positions(sims, transform_key=self.reg_transform_key,
                                      use_positional_colors=False, view_labels=file_labels, view_labels_size=3,
                                      show_plot=self.mpl_ui, output_filename=registered_positions_filename)
 
@@ -228,7 +228,7 @@ class MVSRegistration:
         extra_metadata = params.get('extra_metadata', {})
         z_scale = extra_metadata.get('scale', {}).get('z')
 
-        sources = [create_dask_source(file) for file in filenames]
+        sources = [create_dask_source(file, source_metadata) for file in filenames]
         source0 = sources[0]
         images = []
         sims = []
@@ -252,28 +252,6 @@ class MVSRegistration:
             scale = source.get_pixel_size()
             translation = source.get_position()
             rotation = source.get_rotation()
-            if isinstance(source_metadata, dict):
-                filename_numeric = find_all_numbers(filename)
-                filename_dict = {key: int(value) for key, value in split_numeric_dict(filename).items()}
-                context = {'filename_numeric': filename_numeric, 'fn': filename_numeric} | filename_dict
-                if 'position' in source_metadata:
-                    translation0 = source_metadata['position']
-                    if 'x' in translation0:
-                        translation['x'] = eval_context(translation0, 'x', 0, context)
-                    if 'y' in translation0:
-                        translation['y'] = eval_context(translation0, 'y', 0, context)
-                    if 'z' in translation0:
-                        translation['z'] = eval_context(translation0, 'z', 0, context)
-                if 'scale' in source_metadata:
-                    scale0 = source_metadata['scale']
-                    if 'x' in scale0:
-                        scale['x'] = eval_context(scale0, 'x', 1, context)
-                    if 'y' in scale0:
-                        scale['y'] = eval_context(scale0, 'y', 1, context)
-                    if 'z' in scale0:
-                        scale['z'] = eval_context(scale0, 'z', 1, context)
-                if 'rotation' in source_metadata:
-                    rotation = source_metadata['rotation']
 
             if target_scale:
                 pyramid_level = np.argmin(abs(np.array(source.scales) - target_scale))
@@ -353,8 +331,8 @@ class MVSRegistration:
             if len(sim.chunksizes.get('x')) == 1 and len(sim.chunksizes.get('y')) == 1:
                 sim = sim.chunk(convert_xyz_to_dict(chunk_size))
             sims.append(sim)
-            scales2.append([scale[dim] for dim in 'xyz'])
-            translations2.append([translation[dim] for dim in 'xyz'])
+            scales2.append(dict_to_list(scale))
+            translations2.append(dict_to_list(translation))
         return sims, scales2, translations2, rotations
 
     def validate_overlap(self, sims, labels, is_stack=False, expect_large_overlap=False):

src/registration_methods/RegistrationMethodSkFeatures.py
@@ -20,10 +20,10 @@ class RegistrationMethodSkFeatures(RegistrationMethod):
     def __init__(self, source_type):
         super().__init__(source_type)
         #self.feature_model = SIFT(c_dog=0.1 / 3)
-        self.feature_model = ORB(n_keypoints=5000)
+        self.feature_model = ORB(n_keypoints=5000, downscale=np.sqrt(2))
         #self.feature_model = BRIEF()    # no keypoint detection, only descriptor extraction
 
-        self.label = 'matches_ORB_'
+        self.label = 'matches_ORB_scale_'
         self.counter = 0
 
     def detect_features(self, data0):
@@ -53,6 +53,7 @@ class RegistrationMethodSkFeatures(RegistrationMethod):
         fixed_points, fixed_desc = self.detect_features(fixed_data)
         moving_points, moving_desc = self.detect_features(moving_data)
         threshold = get_mean_nn_distance(fixed_points, moving_points) * 10
+        #threshold = 50
         #logging.info(fixed_data.attrs.get('label', '') + ' - ' + str(moving_data.attrs.get('label', '')))
 
         matches = match_descriptors(fixed_desc, moving_desc, cross_check=True, max_ratio=0.92)

src/util.py
@@ -9,8 +9,6 @@ import os
 import re
 from scipy.spatial.transform import Rotation
 from sklearn.neighbors import KDTree
-#from tifffile import xml2dict
-#import yaml
 
 
 def get_default(x, default):
@@ -24,6 +22,10 @@ def ensure_list(x) -> list:
         return x
     else:
         return [x]
+
+
+def dict_to_list(dct, keys = 'xyz'):
+    return [dct[key] for key in keys if key in dct]
 
 
 def reorder(items: list, old_order: str, new_order: str, default_value: int = 0) -> list:
@@ -346,7 +348,7 @@ def create_transform0(center=(0, 0), angle=0, scale=1, translate=(0, 0)):
 
 def create_transform(center, angle, matrix_size=3):
     if isinstance(center, dict):
-        center = [center[dim] for dim in 'xyz']
+        center = dict_to_list(center)
     if len(center) == 2:
         center = np.array(list(center) + [0])
     if angle is None: