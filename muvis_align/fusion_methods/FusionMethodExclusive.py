9763e550705eb9ce552d449b9699e8cc0b2dc046
Joost de Folter <folterj@gmail.com>
2025-12-30 12:35:12 +0100

    Fix custom fusion for multiple planes

--- muvis_align/fusion_methods/FusionMethodExclusive.py
+++ muvis_align/fusion_methods/FusionMethodExclusive.py
@@ -18,16 +18,24 @@ class FusionMethodExclusive(FusionMethod):
         ndarray
             Fusion of input views
         """
+        needs_cleanup = False
         if len(transformed_views) > 1:
+            needs_cleanup = True
             weights = np.zeros(transformed_views.shape, dtype=bool)
             mask = ~np.isnan(transformed_views)
-            # weigh smallest (specialized) shape highest
-            if np.count_nonzero(mask[0]) < np.count_nonzero(mask[1]):
-                weights[0][mask[0]] = True
-                weights[1][~mask[0]] = True
-            else:
-                weights[0][~mask[1]] = True
-                weights[1][mask[1]] = True
+            rem_mask = np.ones(transformed_views.shape[1:], dtype=bool)
+            n = np.count_nonzero(mask, axis=(1,2))
+            indices = np.argsort(n)
+            # prioritise smallest shapes
+            for index in indices:
+                mask1 = mask[index]
+                weights[index][mask1 * rem_mask] = True
+                rem_mask *= ~mask1
             transformed_views *= weights
 
-        return np.nansum(transformed_views, axis=0).astype(transformed_views[0].dtype)
+        fused = np.nansum(transformed_views, axis=0).astype(transformed_views[0].dtype)
+        if needs_cleanup:
+            del weights
+            del mask
+            del rem_mask
+        return fused
