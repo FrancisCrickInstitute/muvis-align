41be3691e5e476782f91a9d66aa655ef378e0364
Joost de Folter <folterj@gmail.com>
2026-01-05 11:33:41 +0100

    Added fusion method documentation

--- muvis_align/fusion_methods/FusionMethodExclusive.py
+++ muvis_align/fusion_methods/FusionMethodExclusive.py
@@ -6,7 +6,9 @@ from muvis_align.fusion_methods.FusionMethod import FusionMethod
 class FusionMethodExclusive(FusionMethod):
     def fusion(self, transformed_views):
         """
-        Simple exclusive fusion.
+        Simple exclusive fusion:
+        1. in case of mixed pixel sizes: do not blend, in lieu of knowing actual pixel sizes prioritise 'smallest' (non-NaN) tiles first
+        2. avoid blending in general - exclusive use of single source information
 
         Parameters
         ----------
@@ -21,16 +23,21 @@ class FusionMethodExclusive(FusionMethod):
         needs_cleanup = False
         if len(transformed_views) > 1:
             needs_cleanup = True
+            # create exclusive weights map for all views
             weights = np.zeros(transformed_views.shape, dtype=bool)
+            # mask with all non-NaN pixels
             mask = ~np.isnan(transformed_views)
+            # remaining mask (single mask for all views)
             rem_mask = np.ones(transformed_views.shape[1:], dtype=bool)
             n = np.count_nonzero(mask, axis=(1,2))
             indices = np.argsort(n)
-            # prioritise smallest shapes
+            # prioritise smallest shapes (in lieu of actual pixel size info)
             for index in indices:
                 mask1 = mask[index]
                 weights[index][mask1 * rem_mask] = True
+                # remove already assigned pixels from remaining mask
                 rem_mask *= ~mask1
+            # apply weights
             transformed_views *= weights
 
         fused = np.nansum(transformed_views, axis=0).astype(transformed_views[0].dtype)
