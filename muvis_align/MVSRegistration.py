Add global gaussian, removed reg normalisation, various pixel size fixes

muvis_align/MVSRegistration.py
@@ -3,13 +3,13 @@
 
 from contextlib import nullcontext
 import dask
-import numpy as np
 from dask.diagnostics import ProgressBar
 import logging
 import multiview_stitcher
 from multiview_stitcher import registration, vis_utils
 from multiview_stitcher.mv_graph import NotEnoughOverlapError
 from multiview_stitcher.registration import get_overlap_bboxes, sims_to_intrinsic_coord_system
+import numpy as np
 import os.path
 import shutil
 from skimage.transform import resize
@@ -474,17 +474,18 @@ class MVSRegistration:
     def preprocess(self, sims, params):
         flatfield_quantiles = params.get('flatfield_quantiles')
         normalisation = params.get('normalisation', '')
+        gaussian_sigma = params.get('gaussian_sigma')
         filter_foreground = params.get('filter_foreground', False)
 
         # normalise pixel size: take max pixel size
-        scale = {dim: max(source.get_pixel_size()[dim] for source in self.sources)
-                 for dim in sims[0].dims if dim in 'xy'}
+        max_scale = {dim: max(scale[dim] for scale in self.scales) for dim in 'xy'}
         needs_reinit = False
         for source in self.sources:
-            if not np.all([np.isclose(source.get_pixel_size()[dim], scale[dim]) for dim in 'xy']):
+            if not np.all([np.isclose(source.get_pixel_size()[dim], max_scale[dim]) for dim in 'xy']):
                 needs_reinit = True
+        scales0 = self.scales
         if needs_reinit:
-            sims = self.init_sims(target_scale=scale)
+            sims = self.init_sims(target_scale=max_scale)
 
         if filter_foreground:
             foreground_map = calc_foreground_map(sims)
@@ -508,9 +509,18 @@ class MVSRegistration:
                 logging.info('Normalising (global)...')
             else:
                 logging.info('Normalising (individual)...')
-            new_sims = normalise(sims, self.source_transform_key, use_global=use_global)
+            new_sims = normalise_sims(sims, self.source_transform_key, use_global=use_global)
         else:
             new_sims = sims
+
+        if gaussian_sigma:
+            new_sims2 = []
+            for sim, scale0 in zip(new_sims, scales0):
+                # factor in original pixel size for gaussian sigma value
+                scale = np.mean(list(scale0.values())) / np.mean(list(max_scale.values()))
+                sigma = gaussian_sigma * (scale ** (1 / 3))
+                new_sims2.append(gaussian_filter_sim(sim, self.source_transform_key, sigma))
+            new_sims = new_sims2
 
         if filter_foreground:
             logging.info('Filtering foreground images...')
@@ -851,11 +861,10 @@ class MVSRegistration:
                 #logging.warning(f'Failed to calculate resolution metric')
         return {'ncc': nccs, 'ssim': ssims}
 
-    def get_overlap_images(self, sim1, sim2, transform_key):
+    def get_overlap_images(self, sim1, sim2, transform_key, overlap_tolerance=0):
         sims = [sim1.squeeze(), sim2.squeeze()]
         # functionality copied from registration.register_pair_of_msims()
         spatial_dims = si_utils.get_spatial_dims_from_sim(sim1)
-        overlap_tolerance = {dim: 0.0 for dim in spatial_dims}
         lowers, uppers = get_overlap_bboxes(
             sims[0],
             sims[1],

muvis_align/image/util.py
@@ -2,7 +2,7 @@ import cv2 as cv
 import numpy as np
 from multiview_stitcher import msi_utils, param_utils, fusion, mv_graph
 from multiview_stitcher import spatial_image_utils as si_utils
-from scipy.ndimage import gaussian_filter
+from skimage.filters import gaussian
 from skimage.feature import plot_matched_features
 from skimage.transform import downscale_local_mean
 from xarray import DataTree
@@ -456,18 +456,14 @@ def create_compression_filter(compression: list) -> tuple:
     return compressor, compression_filters
 
 
-def blur_image_single(image, sigma):
-    return gaussian_filter(image, sigma)
-
-
-def blur_image(image, sigma):
+def gaussian_filter_image(image, sigma):
     nchannels = image.shape[2] if image.ndim == 3 else 1
     if nchannels not in [1, 3]:
         new_image = np.zeros_like(image)
         for channeli in range(nchannels):
-            new_image[..., channeli] = blur_image_single(image[..., channeli], sigma)
+            new_image[..., channeli] = gaussian(image[..., channeli], sigma)
     else:
-        new_image = blur_image_single(image, sigma)
+        new_image = gaussian(image, sigma, preserve_range=True)
     return new_image
 
 
@@ -645,7 +641,7 @@ def calc_foreground_map(sims):
     return map
 
 
-def normalise(sims, transform_key, use_global=True):
+def normalise_sims(sims, transform_key, use_global=True):
     new_sims = []
     dtype = sims[0].dtype
     # global mean and stddev
@@ -669,7 +665,8 @@ def normalise(sims, transform_key, use_global=True):
         if not use_global:
             min = np.mean(sim, dtype=np.float32)
             range = np.std(sim, dtype=np.float32)
-        image = (sim - min) / range
+        #image = (sim - min) / range
+        image = ((sim - min) / range + 1) / 2   # extended range
         image = float2int_image(image.clip(0, 1), dtype)    # np.clip(image) is not dask-compatible, use image.clip() instead
         new_sim = si_utils.get_sim_from_array(
             image,
@@ -683,6 +680,22 @@ def normalise(sims, transform_key, use_global=True):
         )
         new_sims.append(new_sim)
     return new_sims
+
+
+def gaussian_filter_sim(sim, transform_key, sigma):
+    image = np.asarray(sim)
+    blurred_image = gaussian_filter_image(image, sigma)
+    new_sim = si_utils.get_sim_from_array(
+        blurred_image.astype(sim.dtype),
+        dims=sim.dims,
+        scale=si_utils.get_spacing_from_sim(sim),
+        translation=si_utils.get_origin_from_sim(sim),
+        transform_key=transform_key,
+        affine=si_utils.get_affine_from_sim(sim, transform_key),
+        c_coords=sim.c.data,
+        t_coords=sim.t.data
+    )
+    return new_sim
 
 
 def get_sim_physical_size(sim):

muvis_align/registration_methods/RegistrationMethodSkFeatures.py
@@ -7,7 +7,6 @@ import logging
 import numpy as np
 #import SimpleITK as sitk
 from skimage.feature import match_descriptors, SIFT, ORB
-from skimage.filters import gaussian
 from skimage.measure import ransac
 from skimage.transform import AffineTransform, EuclideanTransform
 from spatial_image import SpatialImage
@@ -21,7 +20,7 @@ class RegistrationMethodSkFeatures(RegistrationMethod):
     def __init__(self, source, params, debug=False):
         super().__init__(source, params, debug=debug)
         self.method = params.get('name', 'sift').lower()
-        self.full_size_gaussian_sigma = params.get('gaussian_sigma', params.get('sigma', 1))
+        self.full_size_gaussian_sigma = params.get('gaussian_sigma', params.get('sigma', 0))
         self.downscale_factor = params.get('downscale_factor', params.get('downscale', np.sqrt(2)))
         self.nkeypoints = params.get('max_keypoints', 5000)
         self.cross_check = params.get('cross_check', True)
@@ -50,9 +49,8 @@ class RegistrationMethodSkFeatures(RegistrationMethod):
             # make data 2D
             data0 = data0.max('z')
         data = self.convert_data_to_float(data0)
-        data = norm_image_variance2(data)
         if gaussian_sigma:
-            data = gaussian(data, sigma=gaussian_sigma)
+            data = gaussian_filter_image(data, gaussian_sigma)
 
         try:
             # not thread-safe - create instance that is not re-used in other thread
@@ -196,7 +194,7 @@ class RegistrationMethodSkFeatures(RegistrationMethod):
 
             draw_keypoints_matches(fixed_data2, fixed_points,
                                    moving_data2, moving_points,
-                                   matches, inliers,
+                                   matches, inliers, points_color='blue',
                                    show_plot=True)
 
         if quality == 0 or np.sum(inliers) == 0:

tests/dask_test.py
@@ -20,7 +20,7 @@ import zarr
 
 from muvis_align.Timer import Timer
 from muvis_align.image.source_helper import create_dask_source
-from muvis_align.image.util import normalise
+from muvis_align.image.util import normalise_sims
 from muvis_align.util import xyz_to_dict
 
 
@@ -147,7 +147,7 @@ def task(filenames):
 
     with Timer('normalise', auto_unit=False):
         #value = np.mean(dask_data).compute()
-        new_sims = normalise(sims, transform_key=transform_key)
+        new_sims = normalise_sims(sims, transform_key=transform_key)
     with Timer('computing', auto_unit=False):
         for new_sim in new_sims:
             new_sim.compute()